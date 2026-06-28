e1cedfc1d93d6146decc7b1249ab48f22987f2c7
Gami <68544656+Loadux@users.noreply.github.com>
2026-06-28 23:34:00 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "gami-math-v1";
+const ASSETS_TO_CACHE = [
+  "./index.html",
+  "./gami.png",
+  "./manifest.json",
+  "https://d3js.org/d3.v7.min.js",
+  "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
